de42525b5166e826e874adbacdefb0aa2c04d837
Paulo Moura <pmoura@logtalk.org>
2019-09-23 22:43:17 +0100

    Add uninstall script

---
+++ logtalk3/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+$packageName = 'logtalk'
+$installerType = 'EXE'
+$silentArgs = '/SILENT'
+$validExitCodes = @(0,1)
+
+try {
+	# HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Logtalk_is1
+	$file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Logtalk_is1").UninstallString
+	Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+  
+	Write-ChocolateySuccess $package
+} catch {
+	Write-ChocolateyFailure $package "$($_.Exception.Message)" 
+	throw
+}
