88c76984aaf722b9a01c64bf48e75e23c8e5382f
Paulo Moura <pmoura@logtalk.org>
2019-09-23 22:06:09 +0100

    Add support files for creating a Logtalk 3.x package

---
+++ logtalk3/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = 'logtalk'
+  fileType       = 'exe'
+
+  checksum       = 'b8dc9c3f331c211ed2372d01ab4efd791110b637242235f9c7fe091db987e6cc'
+  checksumType   = 'sha256'
+  url            = 'https://logtalk.org/files/logtalk-3.30.0.exe'
+
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
