dda4748a25e6acbcec6cd24262a06c3e10a554bc
Paulo Moura <pmoura@logtalk.org>
2019-10-15 17:19:21 +0100

    Update nuspec file for Logtalk 3.31.0 stable

--- logtalk3/tools/chocolateyInstall.ps1
+++ logtalk3/tools/chocolateyInstall.ps1
@@ -4,9 +4,9 @@ $packageArgs = @{
   packageName    = 'logtalk'
   fileType       = 'exe'
 
-  checksum       = '6937fef5f3d36b747673621dd355a98425d47deff2e99670e9fe173c714031a3'
+  checksum       = '5fcdee64dd33690813e43e75ee9968fcdf9093b6f4869726ba1c3a73b2dd83cf'
   checksumType   = 'sha256'
-  url            = 'https://logtalk.org/files/logtalk-3.31.0-b01.exe'
+  url            = 'https://logtalk.org/files/logtalk-3.31.0.exe'
 
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
