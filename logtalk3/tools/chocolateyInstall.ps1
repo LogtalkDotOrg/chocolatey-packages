eea8774ad0222a30fc493d81e2ab50feb9d445c0
Paulo Moura <pmoura@logtalk.org>
2023-07-25 10:06:36 +0100

    Update to Logtalk 3.68.0

--- logtalk3/tools/chocolateyInstall.ps1
+++ logtalk3/tools/chocolateyInstall.ps1
@@ -4,9 +4,9 @@ $packageArgs = @{
   packageName    = 'logtalk'
   fileType       = 'exe'
 
-  checksum       = '1b6d729623e1850c8d741661d8b72ef675f6358573ccbbb2294557d0d8e179ad'
+  checksum       = '2c6124be538641e350cdb24d5b8d3d8179b3b13d2526d3566f5c43e233cd15cb'
   checksumType   = 'sha256'
-  url            = 'https://logtalk.org/files/logtalk-3.67.0.exe'
+  url            = 'https://logtalk.org/files/logtalk-3.68.0.exe'
 
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
